d41bd42a953883dbce8cdfe674868826d17c2f5a
chopraji94 <41725998+chopraji94@users.noreply.github.com>
2020-11-02 22:30:11 +0530

    Error handling for the api

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -16,26 +16,34 @@ class WorldTime {
 
   Future<void> getTime() async{
 
-    //get Request
-    Response response = await get('http://worldtimeapi.org/api/timezone/$url');
-    Map data = jsonDecode(response.body);
-    // print(data);
+    try {
+      //get Request
+      Response response = await get('http://worldtimeapi.org/api/timezone/$url');
+      Map data = jsonDecode(response.body);
+      // print(data);
 
-    //get Poperties from data
-    String dateTime = data['datetime'];
-    String offsetHours = data['utc_offset'].substring(1,3);
-    String offsetMinutes = data['utc_offset'].substring(4,6);
-    // print(dateTime);
-    // print(offsetHours);
-    // print(offsetMinutes);
+      //get Poperties from data
+      String dateTime = data['datetime'];
+      String offsetHours = data['utc_offset'].substring(1,3);
+      String offsetMinutes = data['utc_offset'].substring(4,6);
+      // print(dateTime);
+      // print(offsetHours);
+      // print(offsetMinutes);
 
-    //create DateTime object
-    DateTime now = DateTime.parse(dateTime);
-    now = now.add(Duration(hours: int.parse(offsetHours),minutes: int.parse(offsetMinutes)));
-    print(now);
+      //create DateTime object
+      DateTime now = DateTime.parse(dateTime);
+      now = now.add(Duration(hours: int.parse(offsetHours),minutes: int.parse(offsetMinutes)));
+      print(now);
 
-    //set time property
-    time = now.toString();
+      //set time property
+      time = now.toString();
+    }
+    catch ( e ) {
+      print('Caught error: $e');
+      time = 'Could not get time data';
+    }
+
+
   }
 
 }
